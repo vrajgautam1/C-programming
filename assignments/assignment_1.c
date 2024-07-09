44b937d2012f791eb9245e451f4e62c58180df43
vrajgautam1 <vrajgautam123@gmail.com>
2024-07-10 00:17:28 +0530

    assignments done

---
+++ assignments/assignment_1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+// problem-1
+// void main(){
+//     printf("--------\n");
+//     printf("|\t\t|\n");
+//     printf("R\t\t|\n");
+//     printf("N\t\t|\n");
+//     printf("W\t\t|\n");
+//     printf("|\t\t|\n");
+//     printf("--------\n");
+// }
+
+// problem-2
+// void main(){
+//     printf("*\n");
+//     printf("**\n");
+//     printf("***\n");
+//     printf("**\n");
+//     printf("*\n");
+// }
+
+// problem-3
+void main(){
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*     **      *\n");   
+    printf("*    *  *    *\n");
+    printf("*   *    *  *\n");
+    printf("*  *      **\n");
+    printf("* *\n");
+    printf("*\n");
+}
