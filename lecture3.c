3183b9d83a629251e53286d2c5370e49bc6d23f0
vrajgautam1 <vrajgautam123@gmail.com>
2024-07-11 11:55:08 +0530

    done

--- lecture3.c
+++ lecture3.c
@@ -8,5 +8,16 @@ int main(){
     int c = num1 * num2;
     int d = num1 / num2;
     int e = num1 % num2;
-    printf("addition is:%d \nSubtraction is:%d \nMultiplication is:%d \nDivision is:%d \nModulo is:%d", a, b, c, d, e);
+    printf("addition is:%d \nSubtraction is:%d \nMultiplication is:%d \nDivision is:%d \nModulo is:%d\n", a, b, c, d, e);
+    printf("---------\n|\t\t|\n|5x5=25 |\n|\t\t|\n---------");
+
+    
+
+    char *name = "Vraj Gautam";
+    int age = 25;
+    float height = 5.7;
+    char bloodgroup = 'A';
+    printf("\nI am %s my, age is %d, and these are my details:\nheight:%.1f feet\nBlood-group:%c",name,age,height,bloodgroup); 
+
+    return 0;
 }
