416d354c9709f171b668f14c47bbbf5ea8798a70
vrajgautam1 <vrajgautam123@gmail.com>
2024-07-11 11:45:54 +0530

    lecture 3 lab work

---
+++ lecture3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int num1 = 12;
+    int num2 = 6;
+    int a = num1 + num2;
+    int b = num1 - num2;
+    int c = num1 * num2;
+    int d = num1 / num2;
+    int e = num1 % num2;
+    printf("addition is:%d \nSubtraction is:%d \nMultiplication is:%d \nDivision is:%d \nModulo is:%d", a, b, c, d, e);
+}
